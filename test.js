dc9ed3f8ea8e02b714539f075f35bb012ce93c27
Ruslan_Trafimovich <ruworv@gmail.com>
2024-08-30 22:18:44 +0300

    new task done

--- test.js
+++ test.js
@@ -1,44 +1,22 @@
-//https://www.codewars.com/kata/54a91a4883a7de5d7800009c/train/javascript
+//https://www.codewars.com/kata/55ecd718f46fba02e5000029/train/javascript
 
 // 📌 DESCRIPTION:
 
-// Your job is to write a function which increments a string, to create a new string.
+// Complete the function that takes two integers (a, b, where a < b) and return an array of all integers between the input parameters, including them.
 
-// If the string already ends with a number, the number should be incremented by 1.
-// If the string does not end with a number. the number 1 should be appended to the new string.
-// Examples:
+// For example:
 
-// foo -> foo1
-
-// foobar23 -> foobar24
-
-// foo0042 -> foo0043
-
-// foo9 -> foo10
-
-// foo099 -> foo100
-
-// Attention: If the number has leading zeros the amount of digits should be considered.
+// a = 1
+// b = 4
+// --> [1, 2, 3, 4]
 
 // 💡 SOLUTION:
 
-function incrementString(strng) {
-  if (  !isFinite(strng[strng.length-1])) {
-    return strng+'1'
- }
-  let target = [];
-  for (let i = strng.length - 1; i >= 0; i--) {
-    if (!isFinite(strng[i])) {
-      break;
-    }
-    target.push(strng[i]);
-  }
-  let newStr = strng.slice(0, strng.length - target.length);
-  let newTarget = target.reverse().join("");
-  
-  return (
-    newStr + (parseInt(newTarget) + 1).toString().padStart(newTarget.length, 0)
-  );
+function between(a, b) {
+let arr = [];
+for (let i = a; i<=b; i++) {
+  arr.push(i)
+};
+return arr
 }
-
-console.log(incrementString("lmlm0"));
+console.log(between(1,4))
