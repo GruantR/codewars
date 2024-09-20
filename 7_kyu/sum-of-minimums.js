dce14c9665cfb4ba69893ea496443ae72b2f264e
Ruslan_Trafimovich <ruworv@gmail.com>
2024-09-20 16:53:41 +0300

    new task done

--- 7_kyu/sum-of-minimums.js
+++ 7_kyu/sum-of-minimums.js
@@ -19,6 +19,6 @@
 // 💡 SOLUTION:
 
 function sumOfMinimums(arr) {
-  return arr.map(item => Math.min(...item)).reduce((accum, elem) => accum+elem)
-}
-console.log(sumOfMinimums([[7, 9, 8, 6, 2], [6, 3, 5, 4, 3], [5, 8, 7, 4, 5]]))
+    return arr.map(item => Math.min(...item)).reduce((accum, elem) => accum+elem)
+  }
+  console.log(sumOfMinimums([[7, 9, 8, 6, 2], [6, 3, 5, 4, 3], [5, 8, 7, 4, 5]]))
