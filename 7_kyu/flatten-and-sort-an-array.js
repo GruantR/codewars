5202988fa733983caf41afaeda6d5457fb6c2394
Ruslan_Trafimovich <ruworv@gmail.com>
2024-09-26 20:07:12 +0300

    new task done

--- 7_kyu/flatten-and-sort-an-array.js
+++ 7_kyu/flatten-and-sort-an-array.js
@@ -19,6 +19,6 @@
 
 function flattenAndSort(array) {
   
-  return array.flat().sort((a,b)=>a-b);
-}
-console.log(flattenAndSort([[3, 2, 1], [7, 9, 8], [6, 4, 5]]))
+    return array.flat().sort((a,b)=>a-b);
+  }
+  console.log(flattenAndSort([[3, 2, 1], [7, 9, 8], [6, 4, 5]]))
