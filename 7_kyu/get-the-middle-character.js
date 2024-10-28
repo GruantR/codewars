06f7c981514ddab290744e5d1eed694fc40e0178
Ruslan_Trafimovich <ruworv@gmail.com>
2024-10-28 21:14:59 +0300

    new task done

--- 7_kyu/get-the-middle-character.js
+++ 7_kyu/get-the-middle-character.js
@@ -25,8 +25,8 @@
 // 💡 SOLUTION:
 
 function getMiddle(s){
-  let middle = Math.floor(s.length/2);
-  return s.length %2 === 0 ? s.slice(middle-1,middle+1) :s[middle]
-}
-
-console.log(getMiddle('testing'))
+    let middle = Math.floor(s.length/2);
+    return s.length %2 === 0 ? s.slice(middle-1,middle+1) :s[middle]
+  }
+  
+  console.log(getMiddle('testing'))
