d15061e7748ce17544d125e418f5113546147013
Ruslan_Trafimovich <ruworv@gmail.com>
2024-10-31 00:26:20 +0300

    workBeach

--- test.js
+++ test.js
@@ -3,46 +3,105 @@
 
 // 📌 DESCRIPTION:
 
-// "AL-AHLY" and "Zamalek" are the best teams in Egypt, but "AL-AHLY" always wins the matches between them. "Zamalek" managers want to know what is the best match they've played so far.
-
-// The best match is the match they lost with the minimum goal difference. If there is more than one match with the same difference, choose the one "Zamalek" scored more goals in.
-
-// Given the information about all matches they played, return the index of the best match (0-based). If more than one valid result, return the smallest index.
-
-// Example
-// For ALAHLYGoals = [6,4] and zamalekGoals = [1,2], the output should be 1 (2 in COBOL).
-
-// Because 4 - 2 is less than 6 - 1
-
-// For ALAHLYGoals = [1,2,3,4,5] and zamalekGoals = [0,1,2,3,4], the output should be 4.
-
-// The goal difference of all matches are 1, but at 4th match "Zamalek" scored more goals in. So the result is 4 (5 in COBOL).
-
-// Input/Output
-// [input] integer array ALAHLYGoals
-// The number of goals "AL-AHLY" scored in each match.
-
-// [input] integer array zamalekGoals
-// The number of goals "Zamalek" scored in each match. It is guaranteed that zamalekGoals[i] < ALAHLYGoals[i] for each element.
-
-// [output] an integer
-// Index of the best match.
-
 // 💡 SOLUTION:
 
-  function bestMatch(ALAHLYGoals, zamalekGoals) {
-    let arr = [];
-    ALAHLYGoals.forEach((item,index) => arr.push([item - zamalekGoals[index],index,zamalekGoals[index]]))
+function generator(sequencer,...a) {
+  let firstFunc = sequencer(...a);
+  return {
+    next() {
+      return firstFunc();
+    },
+  };
+}
 
-  return arr.sort((a,b)=>{
-    if (a[0] === b[0]) {
-      return b[2]-a[2]
+function dummySeq() {
+  return function () {
+    return "dummy";
+  };
+}
+
+function factorialSeq() {
+  let i = -1;
+  let lastValue = 1;
+  let finalValue = 1;
+  return function () {
+    i++;
+    if (i === 0 || i === 1) {
+      return 1;
     }
-    return a[0]-b[0]
-  })[0][1]
-    
-    
-  }
-  console.log(bestMatch([1, 2, 3, 4, 5],[0, 1, 2, 3, 4] ))
+    finalValue = i * lastValue;
+    lastValue = finalValue;
+    return finalValue;
+  };
+}
 
+function fibonacciSeq() {
+  let i = 0;
+  let startValue = 0;
+  let secondValue = 1;
+  let finalValue = 0;
+
+  return function () {
+    i += 1;
+    if (i === 1) {
+      return 1;
+    }
+    finalValue = startValue + secondValue;
+    startValue = secondValue;
+    secondValue = finalValue;
+
+    return finalValue;
+  };
+}
+
+function rangeSeq(start, step) {
+  let i = -1;
+  let finalValue = start
+  return function(){
+    i++;
+    if (i === 0) {return finalValue}
+    finalValue +=step
+    return finalValue
+  }
+}
+
+function primeSeq() {
+let index = 1
+return function () {
+  index++
+  if (index === 2) {return 2}
+  if (index === 3) {return 3}
+  let pusk = false;
+  while (!pusk) {
+    index+=1
+    for (let i = 2; i<=Math.ceil(index/2); i++) {
+      if (index % i === 0) {break} 
+      if (i === index) {
+        pusk = true;
+        break
+      }
+    }
+    
+    return index
+
+  }
+}
+}
+
+
+
+var seq = generator(primeSeq)
+console.log(seq.next());
+console.log(seq.next());
+console.log(seq.next());
+console.log(seq.next());
+ console.log(seq.next());
+console.log(seq.next());
+console.log(seq.next());
+console.log(seq.next());
+console.log(seq.next());
+console.log(seq.next());
+console.log(seq.next());
+console.log(seq.next());
+console.log(seq.next());
 
